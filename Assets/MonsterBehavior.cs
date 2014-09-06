6289a22ef8aa05e685c5de0ebabefc9a875f4fe5
FanYou <fyou.cmu@gmail.com>
2014-09-06 17:17:44 -0400

    demo
    demo

---
+++ Assets/MonsterBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBehavior : MonoBehaviour {
+
+
+	public float speed = 1f;
+	public bool isWalking = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isWalking)
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		isWalking = false;
+		//Turn dead animation switch on
+		//Kill object after a few seconds?
+	}
+
+	void initSpeed(){
+		speed = 1f;
+	}
+}
