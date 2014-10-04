8cf43eb77fc540e8bbcf0107c8d8a48a6de66ece
Noeyfan <youfan.noey@gmail.com>
2014-10-04 02:30:22 -0400

    finish game play

--- Eyegaze Template/Assets/Scripts/GameControl.cs
+++ Eyegaze Template/Assets/Scripts/GameControl.cs
@@ -10,6 +10,14 @@ public class GameControl : MonoBehaviour {
 	string[] people_name = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
 	//Component[] parts;
 	public GameObject peoplePrefab;
+
+	string[,] resouce_name = {
+		{"Face1", "Face2"},
+		{"Hair1", "Hair2"},
+		{"Eye1", "Eye2"},
+		{"Nose1", "Nose2"},
+		{"Mouth1", "Mouth2"},
+	};
 	// Use this for initialization
 
 	struct peopleSkeleton {
@@ -22,6 +30,7 @@ public class GameControl : MonoBehaviour {
 		people  = new peopleSkeleton[numbOfPeople];
 		InstantiatePeople();
 		GenerateRandom();
+		people[count].p.SetActive(true);
 		//GameObject testPrefab = (GameObject)Resources.LoadAssetAtPath("Assets/Prefabs/WhiteBoard.prefab", typeof(GameObject));
 		//Instantiate(testPrefab);
 	}
@@ -60,121 +69,20 @@ public class GameControl : MonoBehaviour {
 	}
 
 	void Generate(bool b, int i) {
-		GameObject temp;
-		GameObject Inst;
 		//GameObject temp = (GameObject)Resources.LoadAssetAtPath("Assets/Prefabs/Parts/Face1.prefab", typeof(GameObject));
 		//Generate Character
 		//Generate Passport = Info
 		//Generate Info
 		//Generate Dialog
-
-//		if(b) {
-		for(int k = 0; k < 5; k++) {
-			switch(k){
-			case 0: // Change Face
-				switch(Random.Range(0,2)) {
-				case 0:
-					temp = (GameObject)Resources.Load("Face1", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				case 1:
-					temp = (GameObject)Resources.Load("Face2", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				}
-				//temp= GameObject.Find("P_name");
-				//temp.guiText.text = peopel_name[Random.Range(0,peopel_name.Length)];
-				//temp.transform.parent = people[i].p.transform;
-				break;
-			case 1:// Change Hair
-				switch(Random.Range(0,2)) {
-				case 0:
-					temp = (GameObject)Resources.Load("Hair1", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				case 1:
-					temp = (GameObject)Resources.Load("Hair2", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				}
-				break;
-			case 2://Change Eyes
-				switch(Random.Range(0,2)) {
-				case 0:
-					temp = (GameObject)Resources.Load("Eye1", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				case 1:
-					temp = (GameObject)Resources.Load("Eye2", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				}
-				break;
-			case 3://Change Nose
-				switch(Random.Range(0,2)) {
-				case 0:
-					temp = (GameObject)Resources.Load("Nose1", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				case 1:
-					temp = (GameObject)Resources.Load("Nose2", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				}
-				break;
-			case 4:
-				switch(Random.Range(0,2)) {
-				case 0:
-					temp = (GameObject)Resources.Load("Mouth1", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				case 1:
-					temp = (GameObject)Resources.Load("Mouth2", typeof(GameObject));
-					Inst = Instantiate(temp) as GameObject;
-					Inst.transform.parent = people[i].p.transform;
-					break;
-				}
-				GeneratePP(b,i); 				//generate other info
-				break;
+		for(int k =0; k < 5; k++) {
+			GameObject temp = (GameObject)Resources.Load(resouce_name[k, Random.Range(0, 2)], typeof(GameObject));
+			GameObject Inst = Instantiate(temp) as GameObject;
+			Inst.transform.parent = people[i].p.transform;
+			if (k == 4) {
+				GeneratePP(b,i);
 			}
 		}
-
-			/*foreach (Transform parts in people[i].p.transform) {
-				//print ("kid: " + parts.name);
-				switch(parts.name) {
-				case "Face":
-					//GenerateFace(parts.gameObject);
-					switch(Random.Range(0,2)){
-					case 0:
-						parts.gameObject = (GameObject)Resources.LoadAssetAtPath("Assets/Prefabs/Parts/Face1.prefab", typeof(GameObject));
-						break;
-					case 1:
-						parts.gameObject = (GameObject)Resources.LoadAssetAtPath("Assets/Prefabs/Parts/Face2.prefab", typeof(GameObject));
-						break;
-					}
-					break;
-				case "Nose":
-					break;
-				case "Eyes":
-					break;
-				case "Mouth":
-					break;
-				case "Hairs":
-					break;
-				}
-			}
-		*/ //}else {
-			//Genreate False
-		}
+	}
 	//}
 
 	void GeneratePP (bool b, int i) {
@@ -183,8 +91,16 @@ public class GameControl : MonoBehaviour {
 			GameObject passport = (GameObject)Resources.Load("Passport", typeof(GameObject));
 			passport = Instantiate(passport) as GameObject;
 			GameObject clone = Instantiate(people[i].p, passport.transform.position - pOffset, Quaternion.identity) as GameObject;
+			// change clone
 			clone.transform.localScale -= new Vector3(0.6F, 0.6F, 0);
 			clone.SetActive(true);
+			Transform[] allChildren = clone.gameObject.GetComponentsInChildren<Transform>();
+			foreach (Transform children in allChildren) {
+				if(children.name.Contains("Hair")) {
+					print("hair");
+					//children.gameObject.renderer = (Instantiate((GameObject)Resources.Load(resouce_name[1, Random.Range(0, 2)], typeof(GameObject))) as GameObject).renderer;
+				}
+			}
 			clone.transform.parent = passport.transform;
 			passport.transform.parent = people[i].p.transform;
 			passport.SetActive(true);
@@ -202,7 +118,7 @@ public class GameControl : MonoBehaviour {
 	}
 
 	//Useless for now
-	void GenerateFace(GameObject go){// Useless, Cannot pass reference
+	/*void GenerateFace(GameObject go){// Useless, Cannot pass reference
 		print(go.name);
 		switch(Random.Range(0,2)){
 		case 0:
@@ -215,29 +131,30 @@ public class GameControl : MonoBehaviour {
 		//foreach (var face in GameObject.FindGameObjectsWithTag("Face")) {
 		//	face.SetActive(false);
 		//}
-	}
+	}*/
 
 	void NextOne(bool b) {
-		if(count < numbOfPeople) {
-			if(count >= 1) {
-				people[count - 1].p.SetActive(false);
-				people[count - 1].playerChoice = b;
-			}
-			people[count++].p.SetActive(true);
-			GameObject temp= GameObject.Find("P_name");
-			temp.guiText.text = "Name: " + people_name[Random.Range(0,people_name.Length)];
-		}
-		else {
-			for(int i = 0; i < numbOfPeople; i++) {
-				if(people[i].identity == people[i].playerChoice) {
-					print("same");
-					//nothing
-				}else {
-					print("lose");
-					return;
+		if(count < numbOfPeople) 
+		{
+			people[count].p.SetActive(false);
+			people[count++].playerChoice = b;
+			if(count < numbOfPeople){
+				people[count].p.SetActive(true);
+				GameObject temp= GameObject.Find("P_name");
+				temp.guiText.text = "Name: " + people_name[Random.Range(0,people_name.Length)];
+			}	else {
+				for(int i = 0; i < numbOfPeople; i++) {
+					print(people[i].identity +" "+ people[i].playerChoice + i);
+					if(people[i].identity == people[i].playerChoice) {
+						print("same");
+						//nothing
+					}else if(people[i].identity != people[i].playerChoice){
+						print("lose");
+						return;
+					}
 				}
+				print("win");
 			}
-			print("win");
 		}
 	}
 }
