6809ae46ac2ff7d25871d725c6cb3587f3ac3eff
FanYou <youfan.noey@gmail.com>
2014-10-24 11:02:32 -0700

    TV

---
+++ PsychoKiller/Assets/Scripts/TvController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class TvController : MonoBehaviour {
+	//public MovieTexture movT;
+	// Use this for initialization
+	protected bool isPlaying;
+	void Start () {
+		MovieTexture movT =  renderer.material.mainTexture as MovieTexture;
+		audio.clip = movT.audioClip;
+		audio.Play();
+		movT.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlayTv() {
+		isPlaying = true;
+	}
+}
